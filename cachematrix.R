c02a126998baa6b983287f89914b3f4fef2364af
Zach Taylor <zachtaylorstudios@gmail.com>
2014-04-25 20:50:43 -0700

    Implemented both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions to make repeated inversions of a
+## matrix speedy using a cache when using identical matricies
 
-## Write a short comment describing this function
+## Creates a special form of a matrix that can use a cache for inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <- NULL  # New matrix inputed so can't use cache
+    }
+    get <- function() x
+    setinvert <- function(invert) i <<- invert
+    getinvert <- function() i
+    list(set = set, get = get,
+         setinvert = setinvert,
+         getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Inverts the matrix, using a cached solution if possible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinvert()
+    if(!is.null(i)) {
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(x, ...)
+    x$setinvert(i)
+    i
 }
